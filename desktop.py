2dc4e84692b1b23e3af0b919c55e504a0e2e5a28
Tim Lun <Tim.Lun@morningstar.com>
2019-02-26 13:47:21 +0800

    first_commmit

---
+++ desktop.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sklearn
+
+
+# In[3]:
+
+
+import tensorflow as tf
+
+
+# In[4]:
+
+
+import scipy
+
+
+# In[6]:
+
+
+import numpy as np
+
+
+# In[8]:
+
+
+import sklearn
+
+
+# In[9]:
+
+
+import tensorflow as tf
+
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+dif wi():
+    print('hello')
+
+ge
+
+
+# In[ ]:
+
+
+sb
+
+
+# In[ ]:
+
+
+
+
